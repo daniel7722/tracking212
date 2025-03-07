c04c3febbdcdd5e6fef41de98e780849a1dcf4e7
Daniel Huang <74921405+daniel7722@users.noreply.github.com>
2025-03-07 14:59:22 +0800

    feat: Create nextjs project and remove default front page

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Tracking 212</h1>
+      <p>
+        Tracking 212 is a platform for keeping track of your trading journal.
+      </p>
+      <p>You can use it to track your trades, your profits, and your losses.</p>
+      <button>Start Tracking</button>
+    </div>
+  );
+}
